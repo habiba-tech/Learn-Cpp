2534b16f03100d9b057c1b485987f02ec29f62cb
habiba Siddiqui <siddiquiummehabiba41@gmail.com>
2025-09-30 21:00:12 +0530

    Create Userinput.cpp
    Signed-off-by:  habiba Siddiqui <siddiquiummehabiba41@gmail.com>

---
+++ Userinput.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    n=34;
+    cout<<n<<endl;
+    string a = "abcd";
+    bool t = true;
+    cout<<a<<endl;
+    cout<<t<<endl;
+
+    cout << "Enter a Number : ";
+    cin >> n;
+
+    cout << "you have entered This Number : " << n << "\n";
+    cout << "you have entered This Number : " << n << endl;
+    cout << "Hello World!";         
+    return 0;
+}
