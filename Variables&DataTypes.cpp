6a3fc87c2c7eeeb14ab5995b51c30bc915a6f37c
habiba Siddiqui <siddiquiummehabiba41@gmail.com>
2025-10-01 00:16:31 +0530

    Create Variables&DataTypes.cpp
    Signed-off-by:  habiba Siddiqui <siddiquiummehabiba41@gmail.com>

---
+++ Variables&DataTypes.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num = 10;
+    float pi = 3.14;
+    char grade = 'A';
+    string name = "Alice";
+    bool isStudent = true;
+
+    cout << "Number: " << num << endl;
+    cout << "Pi: " << pi << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Name: " << name << endl;
+    cout << "Student? " << isStudent;
+    return 0;
+}
